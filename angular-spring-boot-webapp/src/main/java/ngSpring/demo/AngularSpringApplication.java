c85211790de7bf18a838cbb730bc3e3b02d0006b
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-06-20 13:02:54 +0200

    Update API docs

--- angular-spring-boot-webapp/src/main/java/ngSpring/demo/AngularSpringApplication.java
+++ angular-spring-boot-webapp/src/main/java/ngSpring/demo/AngularSpringApplication.java
@@ -48,18 +48,11 @@ public class AngularSpringApplication extends SpringBootServletInitializer {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Angular SpringBoot Demo API")
-                .description("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum " +
-                        "has been the industry's standard dummy text ever since the 1500s, when an unknown printer "
-                        + "took a " +
-                        "galley of type and scrambled it to make a type specimen book. It has survived not only five " +
-                        "centuries, but also the leap into electronic typesetting, remaining essentially unchanged. " +
-                        "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum " +
-                        "passages, and more recently with desktop publishing software like Aldus PageMaker including " +
-                        "versions of Lorem Ipsum.")
-                .termsOfServiceUrl("https://github.com/hypery2k/angular-spring-boot-sample")
-                .contact("contact@martinreinhardt-online.de")
+                .description("Sample AngularJS web app with SpringBoot REST backend")
+                .termsOfServiceUrl("https://github.com/holisticon/continous-delivery-demo")
+                .contact("info@holisticon.de")
                 .license("MIT")
-                .licenseUrl("https://raw.githubusercontent.com/hypery2k/angular-spring-boot-sample/master/LICENSE")
+                .licenseUrl("https://raw.githubusercontent.com/holisticon/continous-delivery-demo/master/LICENSE")
                 .version("0.2.0")
                 .build();
     } //end::swagger-docs[]
