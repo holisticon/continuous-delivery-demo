33d10e0cd3748f75a660a49e58709a83cb60f776
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-08-29 08:01:12 +0200

    feat(swagger-bean-validation): Enabling Bean Validation support on Swagger

--- angular-spring-boot-webapp/src/main/java/ngSpring/demo/AngularSpringApplication.java
+++ angular-spring-boot-webapp/src/main/java/ngSpring/demo/AngularSpringApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -23,43 +25,44 @@ import static springfox.documentation.builders.PathSelectors.regex;
 //end::spring-app[]
 //tag::swagger-docs[]
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 //tag::spring-app[]
 public class AngularSpringApplication extends SpringBootServletInitializer {
 //end::spring-app[]
 
-    @Bean
-    public Docket ngSpringApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("ng-spring-boot-api")
-                .apiInfo(apiInfo())
-                .select()
-                .paths(apiPaths())
-                .build();
-    }
+  @Bean
+  public Docket ngSpringApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+      .groupName("ng-spring-boot-api")
+      .apiInfo(apiInfo())
+      .select()
+      .paths(apiPaths())
+      .build();
+  }
 
-    private Predicate<String> apiPaths() {
-        return or(
-                regex("/api/event.*"),
-                regex("/api/login.*"),
-                regex("/api/user.*")
-        );
-    }
+  private Predicate<String> apiPaths() {
+    return or(
+      regex("/api/event.*"),
+      regex("/api/login.*"),
+      regex("/api/user.*")
+    );
+  }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Angular SpringBoot Demo API")
-                .description("Sample AngularJS web app with SpringBoot REST backend")
-                .termsOfServiceUrl("https://github.com/holisticon/continous-delivery-demo")
-                .contact("info@holisticon.de")
-                .license("MIT")
-                .licenseUrl("https://raw.githubusercontent.com/holisticon/continous-delivery-demo/master/LICENSE")
-                .version("0.2.0")
-                .build();
-    } //end::swagger-docs[]
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+      .title("Angular SpringBoot Demo API")
+      .description("Sample AngularJS web app with SpringBoot REST backend")
+      .termsOfServiceUrl("https://github.com/holisticon/continous-delivery-demo")
+      .contact("info@holisticon.de")
+      .license("MIT")
+      .licenseUrl("https://raw.githubusercontent.com/holisticon/continous-delivery-demo/master/LICENSE")
+      .version("0.2.0")
+      .build();
+  } //end::swagger-docs[]
 
-    //tag::spring-app[]
-    public static void main(String[] args) {
-        SpringApplication.run(AngularSpringApplication.class, args);
-    }
-    //end::spring-app[]
+  //tag::spring-app[]
+  public static void main(String[] args) {
+    SpringApplication.run(AngularSpringApplication.class, args);
+  }
+  //end::spring-app[]
 }
